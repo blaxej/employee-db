7d6fd16d6d48bc668696bce41a1eac8bf3c1fcc1
Arkadiusz Partyka <partyka.arkadiusz@gmail.com>
2019-07-07 12:02:25 +0200

    - task cannot be assigned to busy employee

--- src/test/java/pl/sda/AssigninTasksTest.java
+++ src/test/java/pl/sda/AssigninTasksTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class AssigninTasksTest {
 
@@ -31,5 +31,58 @@ public class AssigninTasksTest {
 
         /// then
         assertThat(employeeTask.get().getTitle()).isEqualTo(task.getTitle());
+    }
+
+    @DisplayName("given task assigned to employee" +
+            "when trying to assign this task to another employee" +
+            "then assign fail")
+    @Test
+    void assignBusyTaskToAnotherEmployeeFail() {
+        //given
+        EmployeeDB employeeDB = new EmployeeDB();
+        TaskDB taskDB = new TaskDB();
+        TaskService taskService = new TaskService(employeeDB, taskDB);
+        Task task = new Task("fixing bug");
+        // add employee Andrzej...
+        Employee employeeWithTask = new Employee("Andrzej");
+        Employee employeeWithoutTask = new Employee("Bożydar");
+        long employeeWithTaskId = employeeDB.add(employeeWithTask);
+        long employeeWithoutTaskId = employeeDB.add(employeeWithoutTask);
+        long taskId = taskDB.add(task);
+        taskService.assignTaskToEmployee(taskId, employeeWithTaskId);
+
+        //when
+        TaskAlreadyAssignedException exception = catchThrowableOfType(
+                () -> taskService.assignTaskToEmployee(taskId, employeeWithoutTaskId),
+                TaskAlreadyAssignedException.class);
+
+        //then
+        assertThat(exception).isNotNull();
+        assertThat(taskService.getEmployeeTask(employeeWithoutTaskId)).isEmpty();
+    }
+
+    @DisplayName("given employee with task" +
+            "when try assign new task to this employee" +
+            "then assign will fail")
+    @Test
+    void assignTaskToEmployeeWithTaskShouldFail() {
+        //given
+        EmployeeDB employeeDB = new EmployeeDB();
+        TaskDB taskDB = new TaskDB();
+        TaskService taskService = new TaskService(employeeDB, taskDB);
+        Task firstTask = new Task("fixing bug");
+        Task secondTask = new Task("it depends");
+        // add employee Andrzej...
+        Employee employee = new Employee("Andrzej");
+        long employeeId = employeeDB.add(employee);
+        long firstTaskId = taskDB.add(firstTask);
+        long secondTaskId = taskDB.add(secondTask);
+        taskService.assignTaskToEmployee(firstTaskId, employeeId);
+        //when
+        EmployeeAlreadyBusyException exception = catchThrowableOfType(
+                () -> taskService.assignTaskToEmployee(secondTaskId, employeeId), EmployeeAlreadyBusyException.class);
+        //then
+        assertThat(exception).isNotNull();
+        assertThat(taskService.getEmployeeTask(employeeId).get().getTitle()).isEqualTo("fixing bug");
     }
 }
